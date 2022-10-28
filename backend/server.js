8b1d1be109d88725eecb5a80332fab1e7c7da15d
Jim Chang <warrior1744@gmail.com>
2022-10-28 18:38:24 +0800

    add heroku build

--- backend/server.js
+++ backend/server.js
@@ -17,13 +17,19 @@ connectDB()
 app.use(express.json()) //accept json format in the body. replaced bodyParser.json() since the new release
 app.use(express.urlencoded({extended: false}))
 
-app.get('/', (req, res) => {
-    res.send('API is running...')
-})
-
 app.use('/api/bucket', bucketRoutes)
 app.use('/api/users', userRoutes)
 
+const __dirname = path.resolve()
+
+if(process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '/frontend/build')))
+    app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html')))
+}else{
+    app.get('/', (req, res) => {
+    res.send('API is running...')
+})
+}
 
 app.use(notFound)
 app.use(errorHandler)
