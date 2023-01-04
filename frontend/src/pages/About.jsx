c4ec9eb84f0530f64ca5ed323fea763ed4f7db5c
Jim Chang <warrior1744@gmail.com>
2023-01-04 16:21:01 +0800

    update about

--- frontend/src/pages/About.jsx
+++ frontend/src/pages/About.jsx
@@ -8,8 +8,9 @@ function About() {
         A free, AI-powered research tool for scientific literature
         </p>
         <p className="text-lg text-gray-400">
-            Version <span className="text-white">1.0.0</span>
+            Version <span className="text-white">1.0.1</span>
         </p>
+        <p>Updated on Jan 4th 2023</p>
         <p className="text-lg text-gray-400">
             Created By: 
             <a href="https://www.jim1984.com" className="text-white"> Jim Chang
